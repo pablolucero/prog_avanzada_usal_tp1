d907168747ae45c2700eda449097e2401d41e890
plucero <plucero87@gmail.com>
2015-03-26 12:43:20 -0300

    mas tildes removidos

--- ejercicios/Ejercicio3.java
+++ ejercicios/Ejercicio3.java
@@ -9,28 +9,28 @@ public class Ejercicio3 {
 			
 	public static void main(String[] args) {
 		System.out.println("El numero " + 0 + " es primo? " + esPrimo(0));
-		// El n�mero 0 es primo? false
+		// El numero 0 es primo? false
 
 		System.out.println("El numero " + 1 + " es primo? " + esPrimo(1));
-		// El n�mero 1 es primo? false
+		// El numero 1 es primo? false
 
 		System.out.println("El numero " + 2 + " es primo? " + esPrimo(2));
-		// El n�mero 2 es primo? true
+		// El numero 2 es primo? true
 
 		System.out.println("El numero " + 3 + " es primo? " + esPrimo(3));
-		// El n�mero 3 es primo? true
+		// El numero 3 es primo? true
 
 		System.out.println("El numero " + 4 + " es primo? " + esPrimo(4));
-		// El n�mero 4 es primo? false
+		// El numero 4 es primo? false
 
 		System.out.println("El numero " + 5 + " es primo? " + esPrimo(5));
-		// El n�mero 5 es primo? true
+		// El numero 5 es primo? true
 
 		System.out.println("El numero " + 17 + " es primo? " + esPrimo(17));
-		// El n�mero 17 es primo? True
+		// El numero 17 es primo? True
 
 		System.out.println("El numero " + 18 + " es primo? " + esPrimo(18));
-		// El n�mero 18 es primo? true
+		// El numero 18 es primo? true
 
 	}
 
