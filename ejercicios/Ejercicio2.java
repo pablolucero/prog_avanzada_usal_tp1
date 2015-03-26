a0719302d6b704b21c13711a5434550957e1afb6
Pablo Lucero <plucero87@gmail.com>
2015-03-26 03:25:49 -0300

    resolve ej2

--- ejercicios/Ejercicio2.java
+++ ejercicios/Ejercicio2.java
@@ -3,10 +3,23 @@ package ejercicios;
 public class Ejercicio2 {
 
 	public static String numerosParesEnRangoN(int numero) {
-		// TODO
-		return null;
+
+		StringBuilder sb = new StringBuilder();
+
+			int i = 0;
+
+			while (i <= numero) {
+				sb.append(i).append(", ");
+				i = i + 2;
+			}
+			sb.setLength(sb.length() - 2);
+		return sb.toString();
 	}
-			
+
+	private static boolean esPar(int numero) {
+		return numero % 2 == 0;
+	}
+
 	public static void main(String[] args) {
 		int n1 = 7;
 		System.out.println("Números pares entre 0 y " + n1 + ": " + numerosParesEnRangoN(n1));
@@ -16,6 +29,5 @@ public class Ejercicio2 {
 		System.out.println("Números pares entre 0 y " + n2 + ": " + numerosParesEnRangoN(n2));
 		// Números pares entre 0 y 3: 0, 2
 	}
-
-
+	
 }
