71329790ea6f66813989e72001a5cef53ffc807c
plucero <plucero87@gmail.com>
2015-03-26 16:41:07 -0300

    resolve ej5

--- ejercicios/Ejercicio5.java
+++ ejercicios/Ejercicio5.java
@@ -1,14 +1,27 @@
 package ejercicios;
 
+import java.util.Arrays;
+
 public class Ejercicio5 {
 
 	public static int[][] matrizIdentidad(int n) {
-		// TODO
-		return null;
+		int[][] matriz = new int[n][n];
+
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz.length; j++) {
+				if (i == j)
+					matriz[i][j] = 1;
+			}
+		}
+
+		return matriz;
 	}
 
 	public static void mostrarMatriz(int[][] m) {
-		// TODO
+
+		for (final int[] matrixRow : m) {
+			System.out.println(Arrays.toString(matrixRow));
+		}
 	}
 
 	public static void main(String[] args) {
